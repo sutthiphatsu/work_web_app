d82f33c0cb47880ee57b29d826444d37ce63cd06
mo'sy <sutthiphat.su@ku.th>
2026-09-12 11:57:45 +0100

    Invergiveup

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -18,6 +18,15 @@ app.MapGet("/api/todos/{id}", (int id) =>
 {
     var todo = todos.FirstOrDefault(x => x.Id == id);
 
-    return todo;
+    return todo is null ? Results.NotFound() : Results.Ok(todo);
 });
+
+app.MapPost("/api/todos", (TodoPostDto Dto) =>
+{
+    var nextId = todos.Count == 0 ? 1 : todos.Max(x => x.Id) + 1;
+    var newTodo = new TodoGetDto(nextId, Dto.Title, false);
+    todos.Add(newTodo);
+    return Results.Created($"/api/todos/{newTodo.Id}", newTodo);
+});
+
 app.Run();
